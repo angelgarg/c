d1f6a49e37f3f96d68b28e480abac91f9893e311
angelgarg <145918491+angelgarg@users.noreply.github.com>
2024-04-08 13:12:00 +0530

    Update reversearray.c

--- reversearray.c
+++ reversearray.c
@@ -4,11 +4,11 @@ int main()
     int arr[10]={1,2,3,4,5,6,7,8,9,10},brr[10];
     for(int i=0;i<=9;i++)
     {
-        brr[i]=arr[10-i];
+        brr[i]=arr[9-i];
     }
     for(int j=0;j<=9;j++)
     {
-    printf("%d ",&brr[j]);
+    printf("%d ",brr[j]);
     }
     return 0;
 }
